88b425c13d236bee6b24bc594befe0448bdb284f
masukpavel <pasha2000>
2017-11-12 16:32:31 +0200

    First commit

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2016', 'react']
         }
       },
     ]
